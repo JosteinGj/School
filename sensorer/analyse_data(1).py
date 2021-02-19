f8f749a7e0c5b93433a1a014ea05aee8358fcaff
JosteinGj <66584533+JosteinGj@users.noreply.github.com>
2021-02-19 08:16:05 +0100

    Add files via upload

--- sensorer/analyse_data(1).py
+++ sensorer/analyse_data(1).py
@@ -6,7 +6,7 @@ import math as m
 
 
 
-def time_delay(signal_matrix, channels,plot=True):
+def time_delay(signal_matrix, channels, plot=True):
     sig1 = signal_matrix[31250//10:, channels[0]]
     sig2 = signal_matrix[31250//10:, channels[1]]
 
@@ -22,7 +22,7 @@ def time_delay(signal_matrix, channels,plot=True):
     delay = corrmid-np.argmax(np.abs(corr))
 
     if plot:
-        plotwin = 500
+        plotwin = 2000
 
         plt.plot(range(-plotwin, plotwin), corr[len(corr)//2-plotwin:len(corr)//2+plotwin]/max(corr))
         plt.show()
@@ -34,12 +34,12 @@ def direction_detection(data):
     dt = np.ones((3, 3))
     for i in range(3):
         for j in range(3):
-            dt[i, j] = time_delay(data, (i, j))
+            dt[i, j] = time_delay(data, (i, j), True)
     print(dt)
     theta=m.atan2(np.sqrt(3)*(dt[1, 0]+dt[2,0]),(dt[1,0]-dt[2,0]-2*dt[2,1]))
-    print(theta)
+    #print(theta)
     print(np.rad2deg(theta))
-    return theta
+    return np.rad2deg(theta)
 
 def raspi_import(path, channels=5):
     """
@@ -53,38 +53,63 @@ def raspi_import(path, channels=5):
         data = np.fromfile(fid, dtype=np.uint16)
         data = data.reshape((-1, channels))
     return sample_period, data
+uncertangles=[]
+for n in range(10):
 
+    sample_period, data = raspi_import(f'{n}.bin')
 
-
-# Import data from bin file
-sample_period, data = raspi_import('sampling.bin')
-
-data = signal.detrend(data, axis=0)  # removes DC component for each channel
-sample_period *= 1e-6  # change unit to micro seconds
+    data = signal.detrend(data, axis=0)  # removes DC component for each channel
+    sample_period *= 1e-6  # change unit to micro seconds
 
 # Generate time axis
-num_of_samples = data.shape[0]  # returns shape of matrix
-t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
+    num_of_samples = data.shape[0]  # returns shape of matrix
+    t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
 
 # Generate frequency axis and take FFT
-freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
-spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
+    freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
+    spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
 
-direction_detection(data)
+    uncertangles.append(direction_detection(data))
+
+print(uncertangles)
+print(np.min(uncertangles),np.mean(uncertangles),np.max(uncertangles))
+print(np.var(uncertangles),ddof=1)
+
+# Import data from bin file
+for n in range(10,23):
+
+    sample_period, data = raspi_import(f'sampling{n}.bin')
+
+    data = signal.detrend(data, axis=0)  # removes DC component for each channel
+    sample_period *= 1e-6  # change unit to micro seconds
+
+# Generate time axis
+    num_of_samples = data.shape[0]  # returns shape of matrix
+    t = np.linspace(start=0, stop=num_of_samples*sample_period, num=num_of_samples)
+
+# Generate frequency axis and take FFT
+    freq = np.fft.fftfreq(n=num_of_samples, d=sample_period)
+    spectrum = np.fft.fft(data, axis=0)  # takes FFT of all channels
+
+    direction_detection(data)
 
 # Plot the results in two subplots
 # NOTICE: This lazily plots the entire matrixes. All the channels will be put into the same plots.
 # If you want a single channel, use data[:,n] to get channel n
-plt.subplot(2, 1, 1)
-plt.title("Time domain signal")
-plt.xlabel("Time [us]")
-plt.ylabel("Voltage")
-plt.plot(t[31250//10:], data[31250//10:,0])
+    #quit()
+    continue
+    plt.subplot(2, 1, 1)
+    plt.title("Time domain signal")
+    plt.xlabel("Time [us]")
+    plt.ylabel("Voltage")
+    plt.plot(t[31250//10:], data[31250//10:,0])
 
-plt.subplot(2, 1, 2)
-plt.title("Power spectrum of signal")
-plt.xlabel("Frequency [Hz]")
-plt.ylabel("Power [dB]")
-plt.plot(freq, 20*np.log(np.abs(spectrum/np.max(spectrum)))) # get the power spectrum
+    plt.subplot(2, 1, 2)
+    plt.title("Power spectrum of signal")
+    plt.xlabel("Frequency [Hz]")
+    plt.ylabel("Power [dB]")
+    plt.plot(freq, 20*np.log(np.abs(spectrum/np.max(spectrum)))) # get the power spectrum
 
-plt.show()
+    plt.show()
+
+
